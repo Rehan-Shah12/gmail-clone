549f172f2873b5dabede778642c1b7ddd2425823
Rehan-Shah12 <rehannaqvi835@gmail.com>
2024-01-04 20:00:26 +0500

    Test

--- src/components/Compose.js
+++ src/components/Compose.js
@@ -1,6 +1,15 @@
+import "../style/Compose.css"
+
 function Compose(){
+    console.log("Compose Hit")
     return(
-        <div></div>
+        <div className='Compose'>
+            <div><input type='email'/></div>
+            <div><input/></div>
+            <div><textarea/></div>
+
+
+        </div>
     )
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,26 +1,37 @@
-import '../style/Sidebar.css'
+import React, { useState } from 'react';
+import '../style/Sidebar.css';
 import { RiPencilLine } from "react-icons/ri";
-import { FaRegStar } from "react-icons/fa";
-import { IoMailUnread } from "react-icons/io5"
+import { IoMailUnread } from "react-icons/io5";
 import { MdInbox, MdLabelImportantOutline } from "react-icons/md";
+import Compose from "./Compose";
 
 const Sidebar = () => {
+    const [isComposeOpen, setComposeOpen] = useState(false);
+
+    const handleComposeOpening = () => {
+        setComposeOpen(!isComposeOpen);
+    };
+
     return (
         <div className='SideBar'>
-            <div><button className='compose-button'><RiPencilLine size={25} className='compose-icon'/>Compose</button></div>
-            <div className='sidebar-list'>
-                <ul className='options-list'>
-                    <li className='list-active'><MdInbox size={20} className='list-icons'/> Inbox</li>
-                    <li><IoMailUnread size={20} className='list-icons'/> Unread</li>
-                    <li> <MdLabelImportantOutline size={20} className='list-icons'/>Important</li>
-                </ul>
+            <div onClick={handleComposeOpening}>
+                <button className='compose-button'>
+                    <RiPencilLine size={25} className='compose-icon' />
+                    Compose
+                </button>
             </div>
 
+            {isComposeOpen && <Compose />}
 
+            <div className='sidebar-list'>
+                <ul className='options-list'>
+                    <li className='list-active'><MdInbox size={20} className='list-icons' /> Inbox</li>
+                    <li><IoMailUnread size={20} className='list-icons' /> Unread</li>
+                    <li> <MdLabelImportantOutline size={20} className='list-icons' />Important</li>
+                </ul>
+            </div>
         </div>
+    );
+};
 
-
-    )
-}
-
-export default Sidebar
+export default Sidebar;
